c8caebe668f3bacda33172fd0165568b02225cd2
Selena <sele71098@gmail.com>
2025-02-21 23:08:42 +0100

    feat: iterations 1-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
@@ -10,13 +11,12 @@ function App() {
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
-
       <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/students/:studentId" element={<StudentDetailsPage />} />
+            <Route path="/profile" element={<UserProfilePage />} />
+        </Routes>
       </div>
 
     </div>
