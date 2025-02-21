7f1d9635708ab7cbeddbd177807a205c4bdd1379
Selena <sele71098@gmail.com>
2025-02-21 23:31:21 +0100

    feat: iteration 4

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -1,13 +1,13 @@
 import placeholderImage from "./../assets/placeholder.png";
 import studentsData from "./../assets/students.json";
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 function StudentDetailsPage() {
 
   // Find the current student profile by id.
   // In this case, the student with id 1. The `studentId` is hard-coded for now.
   // This could be a URL parameter from React Router, e.g. /students/:studentId
-  const studentId = "1";
+  const { studentId } = useParams();
   const studentProfile = studentsData.find((student) => student._id === studentId);
 
   return (
